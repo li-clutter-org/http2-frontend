ee164b34aff931450433b94084eb523e1a2bd62f
Vladir Parrado Cruz <vladir@zunzun.se>
2015-03-01 15:35:15 +0100

    Writing some doc to have into account in the future.

--- http2/main/views.py
+++ http2/main/views.py
@@ -3,9 +3,14 @@ from rest_framework.response import Response
 
 
 class AnalyzerViewSet(APIView):
+    """
+    This view will set a task in a queue (perhaps using Celery + RabbitMQ),
+    and the task will request the analysis to the URL, and set the data in
+    a place that could be read later to show the results to the user.
+    """
 
     def post(self, request):
-
+        # TODO: call the task that will take of the analysis.
         data = request.DATA
 
         return Response(data, status=status.HTTP_200_OK)
