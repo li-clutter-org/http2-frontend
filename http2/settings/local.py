42118da4de7149eb0d0d3da68ea408ba2627da8e
Vladir Parrado Cruz <vladir@zunzun.se>
2015-04-04 18:28:36 +0200

    Logging settings for all local environments.

--- http2/settings/local.py
+++ http2/settings/local.py
@@ -18,3 +18,37 @@ ANALYSIS_RESULT_PATH = MEDIA_ROOT
 ANALYZER_URL = "http://127.0.0.1:8000/api/analyzer/mocking"
 PROGRESS_PERCENT = 50
 
+# Logging...
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'formatter': 'simple',
+            'class': "logging.StreamHandler",
+            'stream': 'ext://sys.stdout'
+        },
+
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARNING',
+        },
+        'http2front': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    },
+}
