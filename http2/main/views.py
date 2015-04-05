b7c6a6eb9aa1ab09b3a35ba9f31f7bf82296d0a0
Vladir Parrado Cruz <vladir@zunzun.se>
2015-04-05 16:12:37 +0200

    Better exception handling.

--- http2/main/views.py
+++ http2/main/views.py
@@ -148,80 +148,92 @@ class GetAnalysisState(APIView):
     """
 
     def get(self, request, analysis_id):
+        logger = lg.getLogger("http2front")
         try:
             analysis = AnalysisInfo.objects.get(analysis_id=analysis_id)
         except AnalysisInfo.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        result = AnalysisInfoSerializer(analysis).data
-        # otherwise check status via the files
-        if (analysis.state == AnalysisInfo.STATE_SENT or
-                analysis.state == AnalysisInfo.STATE_PROCESSING):
-            progress = {}
-            result_dir = path.join(
-                settings.ANALYSIS_RESULT_PATH, analysis.analysis_id
-            )
-            # if the done file exists
-            if path.exists(
-                    path.join(
-                        result_dir,
-                        settings.ANALYSIS_RESULTS_DONE_FILE_NAME
-                    )
-            ):
-                http1_json_data, http2_json_data = get_har_data_as_json(
-                    result_dir)
+        except Exception as e:
+            message = str(e)
+            logger.error("GetAnalysisState: %s" % message)
+            return Response({"error": message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-                analysis.state = AnalysisInfo.STATE_DONE
-                analysis.http1_json_data = http1_json_data
-                analysis.http2_json_data = http2_json_data
-            elif path.exists(
-                    path.join(
-                        result_dir,
-                        settings.ANALYSIS_RESULTS_FAILED_FILE_NAME
-                    )
-            ):
-                analysis.state = AnalysisInfo.STATE_FAILED
-            elif path.exists(
-                    path.join(
-                        result_dir,
-                        settings.ANALYSIS_RESULTS_PROCESSING_FILE_NAME
-                    )
-            ):
-                # TODO: Mocking the progress info
-                update_progress_mock(analysis)
-
-                progress_file_path = path.join(
-                    result_dir,
-                    settings.ANALYSIS_RESULTS_PROCESSING_FILE_NAME
+        try:
+            result = AnalysisInfoSerializer(analysis).data
+            # otherwise check status via the files
+            if (analysis.state == AnalysisInfo.STATE_SENT or
+                    analysis.state == AnalysisInfo.STATE_PROCESSING):
+                progress = {}
+                result_dir = path.join(
+                    settings.ANALYSIS_RESULT_PATH, analysis.analysis_id
                 )
-                progress_info = open(progress_file_path).read()
-                progress = {'progress': progress_info}  # for now
-                analysis.state = AnalysisInfo.STATE_PROCESSING
-
-                # TODO: 100 % done... just for now, to see the progress and the state change.
-                if int(progress_info) is 100:
-                    http1_json_data, http2_json_data = get_har_data_as_json(result_dir)
+                # if the done file exists
+                if path.exists(
+                        path.join(
+                            result_dir,
+                            settings.ANALYSIS_RESULTS_DONE_FILE_NAME
+                        )
+                ):
+                    http1_json_data, http2_json_data = get_har_data_as_json(
+                        result_dir)
 
                     analysis.state = AnalysisInfo.STATE_DONE
                     analysis.http1_json_data = http1_json_data
                     analysis.http2_json_data = http2_json_data
-            else:
-                # TODO what to do in this case?
-                # Returning the analysis_info data for now, but we should check
-                # this case
-                pass
-            # save new status
-            analysis.save()
-            result = AnalysisInfoSerializer(analysis).data
+                elif path.exists(
+                        path.join(
+                            result_dir,
+                            settings.ANALYSIS_RESULTS_FAILED_FILE_NAME
+                        )
+                ):
+                    analysis.state = AnalysisInfo.STATE_FAILED
+                elif path.exists(
+                        path.join(
+                            result_dir,
+                            settings.ANALYSIS_RESULTS_PROCESSING_FILE_NAME
+                        )
+                ):
+                    # TODO: Mocking the progress info
+                    update_progress_mock(analysis)
 
-            # TODO: More mocking stuffs
-            if analysis.state == AnalysisInfo.STATE_DONE:
-                result.update({
-                    'json': format_json(
-                            ast.literal_eval(str(http1_json_data)),
-                            ast.literal_eval(str(http2_json_data))
+                    progress_file_path = path.join(
+                        result_dir,
+                        settings.ANALYSIS_RESULTS_PROCESSING_FILE_NAME
                     )
-                })
-            if progress:
-                result.update(progress)
+                    progress_info = open(progress_file_path).read()
+                    progress = {'progress': progress_info}  # for now
+                    analysis.state = AnalysisInfo.STATE_PROCESSING
+
+                    # TODO: 100 % done... just for now, to see the progress and the state change.
+                    if int(progress_info) is 100:
+                        http1_json_data, http2_json_data = get_har_data_as_json(result_dir)
+
+                        analysis.state = AnalysisInfo.STATE_DONE
+                        analysis.http1_json_data = http1_json_data
+                        analysis.http2_json_data = http2_json_data
+                else:
+                    # TODO what to do in this case?
+                    # Returning the analysis_info data for now, but we should check
+                    # this case
+                    pass
+                # save new status
+                analysis.save()
+                result = AnalysisInfoSerializer(analysis).data
+
+                # TODO: More mocking stuffs
+                if analysis.state == AnalysisInfo.STATE_DONE:
+                    result.update({
+                        'json': format_json(
+                                ast.literal_eval(str(http1_json_data)),
+                                ast.literal_eval(str(http2_json_data))
+                        )
+                    })
+                if progress:
+                    result.update(progress)
+        except Exception as e:
+            message = str(e)
+            logger.error("GetAnalysisState: %s" % message)
+            return Response({"error": message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         # and return data
         return Response(result)
