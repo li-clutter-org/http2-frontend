5ab8a1fca3aaebf256fbea6dc75b2cb3f2aa03a7
alcidesv <alcidesv@zunzun.se>
2015-04-22 19:46:11 +0200

    Fixed indents in fabric_ec2, I used tabs when creating this file :-(

--- setup/scripts/fabric_ec2.py
+++ setup/scripts/fabric_ec2.py
@@ -37,7 +37,7 @@ if "HTTP2_LOAD_IMPACT__PROJECT_DIR" not in os.environ:
     print("** Could not find HTTP2_LOAD_IMPACT__PROJECT_DIR, maybe you need "
           "   to do `source makeenv` first")
 else:
-	local_project_dir = os.environ["HTTP2_LOAD_IMPACT__PROJECT_DIR"]
+    local_project_dir = os.environ["HTTP2_LOAD_IMPACT__PROJECT_DIR"]
 
 
 hosts_list = [ "ubuntu@" + findout_instance_ips()["serversInstance"] ]
@@ -50,116 +50,116 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def _rsync_subdirs(*subdirs):
-	for subdir_name in subdirs:
-		rsync_project(
-			local_dir = os.path.join( local_project_dir, subdir_name),
-			remote_dir = os.path.join(EC2_REMOTE_DIR,    os.path.dirname(subdir_name))
-			)
+    for subdir_name in subdirs:
+        rsync_project(
+            local_dir = os.path.join( local_project_dir, subdir_name),
+            remote_dir = os.path.join(EC2_REMOTE_DIR,    os.path.dirname(subdir_name))
+        )
 
 
 def rsync_subdirs():
-	_rsync_subdirs("conf", "http2", "run", "setup")
+    _rsync_subdirs("conf", "http2", "run", "setup")
 
 
 def rsync_resources():
-	with lcd(local_project_dir):
-		for filename in  LOCAL_FILES_TO_SYNC:
-			put(
-				local_path  = filename, 
-				remote_path = EC2_REMOTE_DIR
-				)
-		put(
-			local_path = "setup/scripts/staging_run.sh",
-			remote_path = "/usr/bin/env_run",
-			use_sudo = True 
-			)
-		sudo("chmod ugo+x /usr/bin/env_run")
+    with lcd(local_project_dir):
+        for filename in  LOCAL_FILES_TO_SYNC:
+            put(
+                local_path  = filename, 
+                remote_path = EC2_REMOTE_DIR
+            )
+        put(
+            local_path = "setup/scripts/staging_run.sh",
+            remote_path = "/usr/bin/env_run",
+            use_sudo = True 
+        )
+        sudo("chmod ugo+x /usr/bin/env_run")
 
 
 def run_preliminary_script():
-	with cd(EC2_REMOTE_DIR):
-		run(
-			"setup/scripts/preliminary.sh \"{0}\"".format(EC2_REMOTE_DIR)
-		)
+    with cd(EC2_REMOTE_DIR):
+        run(
+            "setup/scripts/preliminary.sh \"{0}\"".format(EC2_REMOTE_DIR)
+        )
 
 
 def config_supervisord():
-	with lcd(local_project_dir):
-		put(
-			local_path="setup/scripts/supervisord_upstart.conf",
-			remote_path="/etc/init/supervisord.conf",
-			use_sudo=True
-		)
-		put(local_path="setup/scripts/supervisord_ec2.conf", 
-			remote_path = os.path.join(
-				EC2_REMOTE_DIR, 
-				"setup/scripts/supervisord_ec2.conf"
-				)
-			)
-	sudo("service supervisord restart")
+    with lcd(local_project_dir):
+        put(
+            local_path="setup/scripts/supervisord_upstart.conf",
+            remote_path="/etc/init/supervisord.conf",
+            use_sudo=True
+        )
+        put(local_path="setup/scripts/supervisord_ec2.conf", 
+            remote_path = os.path.join(
+                EC2_REMOTE_DIR, 
+                "setup/scripts/supervisord_ec2.conf"
+            )
+            )
+    sudo("service supervisord restart")
 
 
 def install_node():
-	# sudo("sudo apt-get update")
-	sudo("sudo apt-get install -y nodejs npm")
-	# For some reason we need a symbolic link
-	sudo("ln -s `which nodejs` /usr/local/bin/node")
+    # sudo("sudo apt-get update")
+    sudo("sudo apt-get install -y nodejs npm")
+    # For some reason we need a symbolic link
+    sudo("ln -s `which nodejs` /usr/local/bin/node")
 
 
 def install_global_python_packages():
-	sudo("apt-get install -y python-pip python-virtualenv python-dev python3-dev")
-	sudo("pip install supervisor --pre")
+    sudo("apt-get install -y python-pip python-virtualenv python-dev python3-dev")
+    sudo("pip install supervisor --pre")
 
 
 def install_newrelic_agent():
-	sudo("env_run pip install newrelic")
+    sudo("env_run pip install newrelic")
 
 
 def install_nginx():
-	sudo("apt-get install -y nginx")
+    sudo("apt-get install -y nginx")
 
 def install_inotify_wait():
-	sudo("apt-get install -y inotify-tools")
+    sudo("apt-get install -y inotify-tools")
 
 
 def configure_nginx():
-	with lcd(local_project_dir):
-		put(
-			local_path  = "conf/prod/vhost.conf",
-			remote_path = "/etc/nginx/sites-available/http2django.conf",
-			use_sudo = True
-		)
-	# Do a symlink
-	sudo("ln -sf /etc/nginx/sites-available/http2django.conf /etc/nginx/sites-enabled/http2django.conf")
-	sudo("service nginx restart")
+    with lcd(local_project_dir):
+        put(
+            local_path  = "conf/prod/vhost.conf",
+            remote_path = "/etc/nginx/sites-available/http2django.conf",
+            use_sudo = True
+        )
+    # Do a symlink
+    sudo("ln -sf /etc/nginx/sites-available/http2django.conf /etc/nginx/sites-enabled/http2django.conf")
+    sudo("service nginx restart")
 
 
 def create_empty_directories():
-	"Logs ..."
-	run("mkdir -p /home/ubuntu/logs/")
-	# Specific logs for Nginx
-	run("mkdir -p /home/ubuntu/logs/http2_prod/")
-	run("mkdir -p /home/ubuntu/logs/supervisord/")
+    "Logs ..."
+    run("mkdir -p /home/ubuntu/logs/")
+    # Specific logs for Nginx
+    run("mkdir -p /home/ubuntu/logs/http2_prod/")
+    run("mkdir -p /home/ubuntu/logs/supervisord/")
 
 def install_ruby_pieces():
-	sudo("gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3")
-	sudo("curl -sSL https://get.rvm.io | bash -s stable")
-	sudo("rvm install ruby-2.0.0-p598")
-	sudo("rvm --default use 2.0")
-	sudo("gem install sass")
+    sudo("gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3")
+    sudo("curl -sSL https://get.rvm.io | bash -s stable")
+    sudo("rvm install ruby-2.0.0-p598")
+    sudo("rvm --default use 2.0")
+    sudo("gem install sass")
 
 
 def install_postgresql_newrelic_plugin():
-	sudo("gem install newrelic_postgres_plugin")
+    sudo("gem install newrelic_postgres_plugin")
 
 
 def configure_postgresql_newrelic_plugin():
-	put(
-		local_path = "setup/scripts/newrelic_plugin.yml",
-		remote_path = "/home/ubuntu/config/newrelic_plugin.yml",
-		use_sudo=True # <-- TODO
-		)
-	# sudo("pg_monitor install -l c98a269a407478b492309396130415711a641ee2")
+    put(
+        local_path = "setup/scripts/newrelic_plugin.yml",
+        remote_path = "/home/ubuntu/config/newrelic_plugin.yml",
+        use_sudo=True # <-- TODO
+    )
+    # sudo("pg_monitor install -l c98a269a407478b492309396130415711a641ee2")
 
 
 def grunt():
@@ -176,68 +176,68 @@ def install_grunt():
 
 
 def complete_remote_deploy():
-	hosts_list = [
-		findout_instance_ips()["serversInstance"]
-	]
-	print( "Proceeding with hosts_list: {0}".format(hosts_list) )
-	with settings(hosts=hosts_list):
-		install_node()
-		install_postgresql()
-		create_empty_directories() # <-- Logging information for example
-		install_ruby_pieces()
-		rsync_resources()
-		run_preliminary_script()
-		install_global_python_packages()
-		install_nginx()
-    	install_grunt()
-    	grunt()
-    	install_newrelic_agent()
-    	config_supervisord()
-    	configure_nginx()
+    hosts_list = [
+        findout_instance_ips()["serversInstance"]
+    ]
+    print( "Proceeding with hosts_list: {0}".format(hosts_list) )
+    with settings(hosts=hosts_list):
+        install_node()
+        install_postgresql()
+        create_empty_directories() # <-- Logging information for example
+        install_ruby_pieces()
+        rsync_resources()
+        run_preliminary_script()
+        install_global_python_packages()
+        install_nginx()
+    install_grunt()
+    grunt()
+    install_newrelic_agent()
+    config_supervisord()
+    configure_nginx()
 
 
 def collect_static():
-	with cd(EC2_REMOTE_DIR):
-		run("env_run python http2/manage.py collectstatic --noinput")
+    with cd(EC2_REMOTE_DIR):
+        run("env_run python http2/manage.py collectstatic --noinput")
 
 
 def syncdb():
-	with cd(EC2_REMOTE_DIR):
-		#run("env_run python http2/manage.py syncdb")
-		run("env_run python http2/manage.py migrate")
+    with cd(EC2_REMOTE_DIR):
+        #run("env_run python http2/manage.py syncdb")
+        run("env_run python http2/manage.py migrate")
 
 
 def resetdb():
-	with cd(EC2_REMOTE_DIR):
-		sudo("""sudo -u postgres psql -U postgres -d postgres -c "DROP DATABASE http2django ;" """
-	    )
-		sudo("""sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE http2django WITH OWNER = http2django;" """
-	    )
+    with cd(EC2_REMOTE_DIR):
+        sudo("""sudo -u postgres psql -U postgres -d postgres -c "DROP DATABASE http2django ;" """
+             )
+        sudo("""sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE http2django WITH OWNER = http2django;" """
+             )
 
 def install_postgresql():
-	sudo("sudo apt-get install -y postgresql postgresql-contrib postgresql-client libpq-dev")
+    sudo("sudo apt-get install -y postgresql postgresql-contrib postgresql-client libpq-dev")
 
 
 def configure_postgresql():
-	sudo("""sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password '{0}';" """.format(
-		passwfile.POSTGRESQL_PASSWORD)
-	)
-	sudo("""sudo -u postgres psql -U postgres -d postgres -c "CREATE USER http2django with password '{0}';" """.format(
-		passwfile.POSTGRESQL_PASSWORD)
-	)
-	sudo("""sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE http2django WITH OWNER = http2django;" """
-	)
+    sudo("""sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password '{0}';" """.format(
+        passwfile.POSTGRESQL_PASSWORD)
+         )
+    sudo("""sudo -u postgres psql -U postgres -d postgres -c "CREATE USER http2django with password '{0}';" """.format(
+        passwfile.POSTGRESQL_PASSWORD)
+         )
+    sudo("""sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE http2django WITH OWNER = http2django;" """
+         )
 
 def refresh_running_state():
-	hosts_list = [
-		findout_instance_ips()["serversInstance"]
-	]
-	fo.local("rm -rf http2/media/*")
-	print( "Proceeding with hosts_list: {0}".format(hosts_list) )
-	with settings(hosts=hosts_list):
-		rsync_subdirs()
-		rsync_resources()
-		grunt()
-    	collect_static()
-		# The one below also happens to trigger a restart of the services...
-    	config_supervisord()
+    hosts_list = [
+        findout_instance_ips()["serversInstance"]
+    ]
+    fo.local("rm -rf http2/media/*")
+    print( "Proceeding with hosts_list: {0}".format(hosts_list) )
+    with settings(hosts=hosts_list):
+        rsync_subdirs()
+        rsync_resources()
+        grunt()
+    collect_static()
+            # The one below also happens to trigger a restart of the services...
+    config_supervisord()
