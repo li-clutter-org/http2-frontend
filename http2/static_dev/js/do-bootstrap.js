942943305f7aa18a5b501db5a21482cb84971231
alcidesv <alcidesv@zunzun.se>
2015-04-23 18:17:46 +0200

    Back to element bootstrap procedure

--- http2/static_dev/js/do-bootstrap.js
+++
@@ -1,4 +0,0 @@
-
-// Ensure that bootstrap doesn't happen before all the modules have been loaded....
-document.addEventListener("DOMContentLoaded", function() {
-        angular.bootstrap(document, ['http2']);  }, false );
