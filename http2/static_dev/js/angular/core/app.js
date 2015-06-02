92a9b51280e14c3478cd6d238d7da9d0d6fcd719
Neyvis Remon <neyvis.remon.gonzalez@gmail.com>
2015-06-01 23:39:40 -0400

    We need this module included here.

--- http2/static_dev/js/angular/core/app.js
+++ http2/static_dev/js/angular/core/app.js
@@ -1,9 +1,9 @@
 'use strict';
-console.info("Executing app.js");
 angular.module('http2', [
     'ui.router',
     'ngCookies',
     'ngMessages',
     'ngSanitize',
-    'angular-growl'
+    'angular-growl',
+    'socialLinks',
 ]);
