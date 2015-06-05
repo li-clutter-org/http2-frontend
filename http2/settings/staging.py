88a91d87f936f42c956fc642821e2854cc4a665c
alcidesv <alcidesv@zunzun.se>
2015-06-05 10:58:24 +0200

    - Some old changes here, not sure what for.

--- http2/settings/staging.py
+++ http2/settings/staging.py
@@ -2,7 +2,7 @@ from .base import *
 
 from logging.handlers import SysLogHandler
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ["http2.httptwo.com"]
 
@@ -10,23 +10,23 @@ ANALYZER_URL = "https://192.168.112.131:1070/setnexturl/"
 
 ANALYSIS_RESULT_PATH="/home/ubuntu/mimic/hars/"
 
-DATABASES = {
-    'default': {
-        'ENGINE'     : 'django.db.backends.postgresql_psycopg2',
-        'NAME'       : 'http2django',
-        'USER'       : 'http2django',
-        'PASSWORD'   : '233feeA4af',
-        'HOST'       : '127.0.0.1',
-        'PORT'       : '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': '/home/ubuntu/http2db'
+#         'ENGINE'     : 'django.db.backends.postgresql_psycopg2',
+#         'NAME'       : 'http2django',
+#         'USER'       : 'http2django',
+#         'PASSWORD'   : '233feeA4af',
+#         'HOST'       : '127.0.0.1',
+#         'PORT'       : '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/ubuntu/http2db'
+    }
+}
 
 LOGGING = {
     'version': 1,
